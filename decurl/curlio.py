865b51cdc8ddb4b61156976e25a6e71bab38a5c3
Grundleyump <joshua.m.weiner@colorado.edu>
2016-06-17 14:47:42 -0600

    added YAML writing/reading

--- decurl/curlio.py
+++ decurl/curlio.py
@@ -1,18 +1,19 @@
 import os, sys, re
 import shlex
 from .parser import CurlParser
+import yaml
 
 def main(fname):
     sys.stdout.write(convert_from_file(fname) + '\n')
 
-def read_config(read_file):
+def read_config(infile):
     """
     Given a path to a .curlrc file, generates and returns
     a command-line curl statement.
     """
     full_cmd = 'curl '
     
-    for line in read_file.readlines():
+    for line in infile.readlines():
         if len(line.strip()) > 0:
             if line.strip()[0] != '#': # if not comment line
                 line = line.strip()
@@ -50,7 +51,37 @@ def write_config(curl_str):
                 result += '{}\n'.format(opt)
 
     return result
-   
+
+def read_config_yaml(infile):
+    """
+    Given a path to a configuration YAML file, generates and returns
+    a command-line curl statement.
+    """
+    args_dict = yaml.load(infile.read())
+
+    full_cmd = 'curl'
+    for opt, val in args_dict.iteritems():
+        if val == True:
+            full_cmd += '--{}'.format(opt)
+        else:
+            full_cmd += ' --{} {}'.format(opt, val)
+
+    return full_cmd
+
+def write_config_yaml(curl_str):
+    """
+    Given a command-line curl statement, generates a YAML file
+    containing a list of all the options and their values.
+    """
+    parser = CurlParser()
+    tokens = shlex.split(curl_str)
+    parsed_args = parser.parse_args(tokens)
+
+    d = vars(parsed_args)
+    # remove keys with False, None, Empty
+    d_clean = {opt: val for opt, val in d.iteritems() if opt != 'command' and val}
+    return yaml.dump(d_clean, default_flow_style=False)
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
